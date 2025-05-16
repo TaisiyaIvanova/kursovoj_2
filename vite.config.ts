c8c1d5b6355a88a3e4abc894aabbdbac1652c39b
tasya <milly8096@gmail.com>
2025-05-16 22:51:36 +0300

    fix: deployment fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/kursovoj_2",
+    base: "/kursovoj_2/",
     plugins: [react()],
 });
